c5369bafb493464dc3fcec04f35a71379b329bb4
ZALMT <dungnguyen10082000@gmail.com>
2023-12-03 11:37:21 +0700

    more work

--- install.sh
+++ install.sh
@@ -29,15 +29,26 @@ cd ~/Downloads
 git clone https://github.com/DMN/Dots && cd Dots
 
 cp -r .config ~/.config cp -r .local ~/.local
+echo "Installing Vim Plug"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 echo "Altering sudoer's bashrc to load bashrc and bash profile files in
 .config"
 
+
 #Placeholder
 sudo echo source /home/zeus/.config/bash/bash_profile>etc/bash.bashrc
 echo "Enabling nvim plugin"
 #Placeholder
+
+vim -c 'PlugInstall'
+
+echo "Installing pandoc and xelatex"
+yay --noconfirm -S pandoc xelatex
+
+
+echo "Installing fonts"
+yay --noconfirm -S gnu-free-fonts nerd-fonts-git nerd-fonts-coomplete-mono-glyphs
 
 echo "Running antidot and xdg ninja" echo "Downloading xdg ninja"
 #Placeholder
@@ -46,5 +57,5 @@ antidot clean
 
 echo "It should be done, now you just have to delete the unused dotfiles in
 your home directory (.bashrc, .bashprofile, .xinitrc and the .nvim directory)
-echo "What's next?" echo "The system should be ready to go, but I'd install the
-Firefox userChrome files in my my repo as well"
+echo "What's next?"
+echo "The system should be ready to go, but I'd install the Firefox userChrome files in my my repo as well"
