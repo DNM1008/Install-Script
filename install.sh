8e01ad6f1719b4662c84d7e47ae8a229b7a24f3f
DNM1008 <dungnguyen10082000@gmail.com>
2024-05-01 05:14:46 +1000

    update

--- install.sh
+++ install.sh
@@ -56,7 +56,7 @@ sudo systemctl enable ly
 sudo systemctl enable cups
 sudo usermod -aG lp $user
 # Enabling Bluetooth
-# sudo systemctl enable bluetooth
+sudo systemctl enable bluetooth
 echo "Cleaning up"
 cd
 
@@ -69,7 +69,6 @@ yay -Scc --noconfirm
 
 antidot update 
 antidot clean 
-eval "$(antidot init)"
 
 echo "The basic setup should be done for now, to get your system to a more functional state, consider install pandoc and texlive, or if it's not what you're looking for, libre office. CUPS has been installed and enabled, however, you probably need to install the specific driver for your printer. To know what driver you need, consult the Arch Wiki. Bluetooth is installed and should be accessible through `bluetoothctl` or `bluetuith`. Consider install evremap to get custom keymaps. I have a config file already in `~/.config/evremap/evremap.toml`. Consult the Arch Wiki to get started. Reboot now for changes to take effect."
 
